5aa52649257ed2f463f7f25ee266e173ec10280a
MakhmudNunu <mahmud.nunu001@gmail.com>
2026-05-10 20:39:29 +0600

    fix: use bcryptjs import

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,7 +8,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
 
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 import { User } from '../users/user.entity';
 
