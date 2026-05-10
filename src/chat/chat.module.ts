a5defde16f41cc97c23f5e0ec60a4c0f840a2521
MakhmudNunu <mahmud.nunu001@gmail.com>
2026-05-10 20:27:34 +0600

    fix: add JwtModule and User entity to ChatModule

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Message } from './entities/message.entity';
+import { User } from '../users/user.entity';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
@@ -8,7 +11,17 @@ import { ChatRoom } from './entities/chat-room.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Message, ChatRoom]),
+    TypeOrmModule.forFeature([Message, ChatRoom, User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '7d',
+        },
+      }),
+    }),
   ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
