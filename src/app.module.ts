595398f2930ae8127162194710369ae394ba1bb6
MakhmudNunu <mahmud.nunu001@gmail.com>
2026-05-10 20:04:57 +0600

    deploy/render

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,15 +14,27 @@ import { Message } from './chat/entities/message.entity';
       envFilePath: '.env',
     }),
 
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: Number(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    //   entities: [User, Message],
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User, Message],
-      synchronize: true,
+      url: process.env.DATABASE_URL,
+      synchronize: true, // ← true для первого запуска (создаст таблицы)
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: true, // ← Render требует SSL для Postgres
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, // ← нужно для Render
+        },
+      },
     }),
 
     AuthModule,
