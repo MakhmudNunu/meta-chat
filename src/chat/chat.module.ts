34d73076ade1dad2643d8ee9f86acd9f567accd4
MakhmudNunu <mahmud.nunu001@gmail.com>
2026-05-10 20:24:37 +0600

    add ChatRoom entity to ChatModule

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,10 +4,11 @@ import { Message } from './entities/message.entity';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { ChatRoom } from './entities/chat-room.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Message]),
+    TypeOrmModule.forFeature([Message, ChatRoom]),
   ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
